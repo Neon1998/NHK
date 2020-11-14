f70853f032f568c6c3b61cd6edb46e982f205f5f
Neon1998 <1449216423@qq.com>
2020-11-14 10:30:27 +0800

    add 1.c

---
+++ 1.c
@@ -0,0 +1,13 @@
+/*
+ * @Author: your name
+ * @Date: 2020-11-14 10:24:17
+ * @LastEditTime: 2020-11-14 10:24:41
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \gitwork\1.c
+ */
+#include<stdio.h>
+int main()
+{
+    printf("hello");
+}
